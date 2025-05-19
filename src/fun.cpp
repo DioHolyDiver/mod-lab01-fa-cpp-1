a3e21e64b880d5cfe393aa813489e58b93fa41fb
DioHolyDiver <163848776+DioHolyDiver@users.noreply.github.com>
2025-05-19 22:32:10 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,11 +2,9 @@
 #include "fun.h"
 #include <iostream>
 #include <cctype>
-
 unsigned int faStr1(const char *str) {
     unsigned int count = 0;
     bool isWord = false;
-    
     while (*str) {
         if (isalpha(*str)) {
             isWord = true;
@@ -22,28 +20,23 @@ unsigned int faStr1(const char *str) {
         }
         str++;
     }
-    
     if (isWord) {
         count++;
     }
     return count;
 }
-
 bool isUpperLatin(char c)
 {
     return (c >= 'A' && c <= 'Z');
 }
-
 bool isLowerLatin(char c)
 {
     return (c >= 'a' && c <= 'z');
 }
-
 unsigned int faStr2(const char *str) {
     unsigned int count = 0;
     bool isWordStart = false;
     bool isValid = true;
-    
     while (*str) {
         if (!isWordStart && isUpperLatin(*str)) {
             isWordStart = true;
@@ -59,21 +52,17 @@ unsigned int faStr2(const char *str) {
             isWordStart = false;
             isValid = true;
         }
-    
         str++;
     }
     return count;
 }
-
 unsigned int faStr3(const char *str) {
     unsigned int totalLength = 0;
     unsigned int wordCount = 0;
-    
     while (*str) {
         while (*str && !isalpha(*str)) {
             str++;
         }
-    
         if (*str) {
             unsigned int wordLength = 0;
             while (*str && isalpha(*str)) {
@@ -84,7 +73,6 @@ unsigned int faStr3(const char *str) {
             wordCount++;
         }
     }
-    
     if (wordCount != 0) {
         return (totalLength + wordCount / 2) / wordCount;
     }
